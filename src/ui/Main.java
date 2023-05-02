51fdf621414ea7230c190324e7c57a9804f4e846
NaySp <nsp1324@hotmail.com>
2023-05-02 12:28:44 -0500

    R1

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,7 +1,11 @@
-package ui; 
+package ui;
 
-import java.util.Scanner; 
-import model.*;
+import com.google.gson.Gson;
+import model.Controller;
+
+import java.io.File;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Main{
 
@@ -19,7 +23,20 @@ public class Main{
     }
 
     public static void main(String[] args){
-        Main view = new Main(); // la llamada al constructor de la clase 
+
+        Main view = new Main(); // la llamada al constructor de la clase
+
+        Gson gson = new Gson();
+        File projectDir = new File(System.getProperty("user.dir"));
+        File dataDirectory = new File(projectDir+"/data");
+        File result = new File(projectDir+"/data/result.json");
+
+        if(!dataDirectory.exists()){
+            dataDirectory.mkdirs();
+            System.out.println(dataDirectory.exists());
+        }
+
+
 
         int option = 0; 
 
@@ -29,30 +46,33 @@ public class Main{
             view.executeOption(option);
 
         }while(option != 0);
-
-
         view.reader.close();
+
     }
 
     public void menu(){
          
-        System.out.println("1. Register Product"); 
-        System.out.println("2. Create order"); 
-        System.out.println("3. Find a product"); 
-        System.out.println("0. Salir"); 
+        System.out.println("1. Register Product \n" +
+                "2. Create order\n" +
+                "3. Find a product\n" +
+                "0. Salir");
+        System.out.print("option -> ");
+
     }
 
     public void executeOption(int option){
         
         switch(option){
         
-            
+            case 1: addProduct();
+            break;
 
             
-            case 0 -> System.out.println("Exit");
-    
+            case 0: System.out.println("Exit");
+            break;
         
-            default -> System.out.println("Invalid Option, try again :c ");
+            default: System.out.println("Invalid Option, try again :c ");
+
         }
 
     }
@@ -69,6 +89,51 @@ public class Main{
         }
         return option; 
     }
+
+    public double validateDouble() {
+        double option = -1;
+        try {
+            option = reader.nextDouble();
+        } catch (InputMismatchException e) {
+            reader.next();
+            System.out.println("Please enter a valid double.");
+        }
+        return option;
+    }
+
+
+
+    public void addProduct(){
+
+        boolean datosCorrectos = false;
+
+        do {
+            System.out.println("Type the name of the product: ");
+            String name = reader.next();
+            reader.nextLine();
+            System.out.println("Type a quick description of the product: ");
+            String productName = reader.next();
+            reader.nextLine();
+            System.out.println("Type the price of the product: ");
+            double price = validateDouble();
+            System.out.println("Type a category: ");
+            String category = reader.next();
+            System.out.println("Finally, type the quantity of this product: ");
+            int numSales = validateIntegerInput();
+
+            if (name.isEmpty() || productName.isEmpty() || category.isEmpty()) {
+                System.out.println("You must enter a value for all fields. Please try again.");
+            } else if (price <= 0 || numSales <= 0) {
+                System.out.println("Price and quantity must be greater than zero. Please try again.");
+            } else {
+                controller.registerProduct(name, productName, price, category, numSales);
+                datosCorrectos = true;
+            }
+        } while (!datosCorrectos);
+        
+    }
+
+
 
     
     
