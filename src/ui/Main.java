141049caa3a7d78c4e741b249e42921fb638246e
Isabella <isabelaocampo08@gmail.com>
2023-05-06 18:11:50 -0500

    fixed testcases and R1

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -121,6 +121,7 @@ public class Main{
 
 
     public void addProduct(){
+        int numSales = 0;
 
         boolean datosCorrectos = false;
 
@@ -133,19 +134,73 @@ public class Main{
             reader.nextLine();
             System.out.println("Type the price of the product: ");
             double price = validateDouble();
-            System.out.println("Type a category: ");
-            String category = reader.next();
-            System.out.println("Finally, type the quantity of this product: ");
-            int numSales = validateIntegerInput();
+            System.out.println("Type the quantity of this product: ");
+            numSales = validateIntegerInput();
 
-            if (name.isEmpty() || productName.isEmpty() || category.isEmpty()) {
+            if (name.isEmpty() || productName.isEmpty() ) {
                 System.out.println("You must enter a value for all fields. Please try again.");
             } else if (price <= 0 || numSales <= 0) {
                 System.out.println("Price and quantity must be greater than zero. Please try again.");
             } else {
-                controller.rProduct(name, productName, price, category, numSales);
-                datosCorrectos = true;
+                System.out.println("Choose a category: \n" +
+                        "1. Libros \n" +
+                        "2. Electrónica \n" +
+                        "3. Ropa y accesorios \n" +
+                        "4. Alimentos y bebidas \n" +
+                        "5. Papelería \n" +
+                        "6.Deportes \n" +
+                        "7. Productos de belleza y cuidado personal \n" +
+                        "8. Juguetes y juegos \n" +
+                        "Option: ");
+                int category = reader.nextInt();
+                if(category < 0 && category > 9){
+                    System.out.println("Choose a valid category");
+                }else {
+                    switch (category) {
+
+                        case 1://Libros
+                            controller.rProduct(name, productName, price, "Libros", numSales);
+                            datosCorrectos = true;
+                            break;
+
+                        case 2://Electronica
+                            controller.rProduct(name, productName, price, "Electronica", numSales);
+                            datosCorrectos = true;
+
+                            break;
+                        case 3://Ropa y accesorios
+                            controller.rProduct(name, productName, price, "Ropa y accesorios", numSales);
+                            datosCorrectos = true;
+                            break;
+
+                        case 4://Alimentos y bebidas
+                            controller.rProduct(name, productName, price, "Alimentos y bebidas", numSales);
+                            datosCorrectos = true;
+                            break;
+
+                        case 5://Papeleria
+                            controller.rProduct(name, productName, price, "Papeleria", numSales);
+                            datosCorrectos = true;
+                            break;
+
+                        case 7://Deportes
+                            controller.rProduct(name, productName, price, "Deportes", numSales);
+                            datosCorrectos = true;
+                            break;
+
+                        case 8://Productos de belleza y cuidado personal
+                            controller.rProduct(name, productName, price, "Productos de belleza y cuidado personal", numSales);
+                            datosCorrectos = true;
+                            break;
+                        case 9:// Juguetes y juegos
+                            controller.rProduct(name, productName, price, "Juguetes y juegos", numSales);
+                            datosCorrectos = true;
+
+                            break;
+                    }
+                }
             }
+
         } while (!datosCorrectos);
         
     }
