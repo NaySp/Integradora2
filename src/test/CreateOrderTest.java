893efab3b0103671f95f66f6b1f1d688bf546e75
Isabella <isabelaocampo08@gmail.com>
2023-05-07 14:59:59 -0500

    failed to write tests

---
+++ src/test/CreateOrderTest.java
@@ -0,0 +1,16 @@
+package test;
+
+import model.*;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+import java.util.List;
+import java.util.Arrays;
+
+public class CreateOrderTest {
+
+    private Controller orderRegistration;
+
+
+
+}
